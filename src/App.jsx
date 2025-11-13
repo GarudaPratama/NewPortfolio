b219a8b106fa5308bffa76ed58cddba40e323e14
GarudaPratama <garuda281209@student.abudzar.sch.id>
2025-11-13 09:16:47 +0700

    Setup (done)

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        🚀 React + Vite + Tailwind CSS v4 Berhasil!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
